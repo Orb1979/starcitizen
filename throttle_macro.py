890bca7aa9882ceef193249a1824b2e38f787a0f
Orb1979 <mail.orb@gmail.com>
2018-01-09 22:20:00 +0100

    Update throttle_macro.py

--- throttle_macro.py
+++ throttle_macro.py
@@ -2,29 +2,28 @@ import gremlin
 from gremlin.spline import CubicSpline
 
 # This script copies front part of the Joysitck y-axis values, to the slider
-# advanteges:
-# - Dont need any form of a split axis, to throttle forward and strafe back
-# - you can see scm speed, (because y-axis forward is copied to slider, which is bind to throttle)
+# advantages:
+# - dont need any form of a split axis, to throttle forward and strafe back
+# - you can see scm speed, (because y-axis forward is copied to the slider)
 # - can use your slider as sort of a trim or base speed
 # - helps with the throttle stuck at 100% issue when going out of decoupled mode (just move your y-axis a bit)
 
+# Star Citizen setup 
 # bind "strafe forward / back" to your Y-axis stick
 # bind "throttle up / down" to your slider
 
-# joystick axis range    = [-1 .. +1] = 0 is no throttle (stick is centered)
-# slider range           = [-1 .. +1] = 0 is throttle 50% (slider in the center)
-
 # Joystick decorator
+# check device information in Gremlin for your (harware id, system id)
 t16000m = gremlin.input_devices.JoystickDecorator(
     name="T.16000M",
-    device_id=(72331530,1), #1 = system id of left joystick, remove if only using 1 joystick
+    device_id=(72331530,1)
     mode="Default"
 )
 
 # set correct axis for joysticks and vjoy output
 inputY          = 2      # joystick axis "Axis 2"
 inputSlider     = 4      # joystick axis "Axis 4"
-slider          = 7      # vjoy axis"7ax"
+slider          = 7      # vjoy axis "7ax"
 
 # set your curve values (curve values can be read from profile xml)
 curve = CubicSpline([
